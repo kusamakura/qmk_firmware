8abc100b2d3cfdc378ff6f8b381aed271b4c8244
kusamakura <882152+kusamakura@users.noreply.github.com>
2022-08-29 03:02:42 +0900

    Add satt/vision/keymaps/peppy

---
+++ keyboards/satt/vision/keymaps/peppy/keymap.c
@@ -0,0 +1,37 @@
+#include QMK_KEYBOARD_H
+
+#include "peppy.h"
+#include "g/keymap_combo.h"
+
+// clang-format off
+#define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
+
+const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_wrapper(
+    KC_MPRV, KC_TAB,  ___LEFT_BASE_ROW2___,           ___RGHT_BASE_ROW2___, KC_BSPC, KC_DEL,
+    KC_MNXT, KC_ESC,  ___LEFT_BASE_ROW3___,           ___RGHT_BASE_ROW3___, KC_ENT,
+             KC_LSFT, ___LEFT_BASE_ROW4___,     XXXX, ___RGHT_BASE_ROW4___, KC_SLSH, KC_RSFT,
+             XXXX, MO(_SYM), NAV_SPC, KC_ENT,   NAV_SPC, XXXX, XXXX,  XXXX
+  ),
+
+  [_NAV] = LAYOUT_wrapper(
+    VVVV, VVVV, ___LEFT_NAV_ROW2___,            ___RGHT_NAV_ROW2___, VVVV, VVVV,
+    VVVV, VVVV, ___LEFT_NAV_ROW3___,            ___RGHT_NAV_ROW3___, VVVV,
+          VVVV, ___LEFT_NAV_ROW4___,      VVVV, ___RGHT_NAV_ROW4___, VVVV, VVVV,
+          VVVV,     VVVV, VVVV, VVVV,     VVVV, VVVV, VVVV,    RESET
+  ),
+
+  [_SYM] = LAYOUT_wrapper(
+    VVVV, VVVV, ___LEFT_SYM_ROW2___,            ___RGHT_SYM_ROW2___, VVVV, VVVV,
+    VVVV, VVVV, ___LEFT_SYM_ROW3___,            ___RGHT_SYM_ROW3___, VVVV,
+          VVVV, ___LEFT_SYM_ROW4___,      VVVV, ___RGHT_SYM_ROW4___, VVVV, VVVV,
+          VVVV,     VVVV, VVVV, VVVV,     VVVV, VVVV, VVVV,    VVVV
+  ),
+
+  [_NUM] = LAYOUT_wrapper(
+    VVVV, VVVV, ___LEFT_NUM_ROW2___,            ___RGHT_NUM_ROW2___, VVVV, VVVV,
+    VVVV, VVVV, ___LEFT_NUM_ROW3___,            ___RGHT_NUM_ROW3___, VVVV,
+          VVVV, ___LEFT_NUM_ROW4___,      VVVV, ___RGHT_NUM_ROW4___, VVVV, VVVV,
+          VVVV,     VVVV, VVVV, VVVV,     VVVV, VVVV, VVVV,    VVVV
+  )
+};
