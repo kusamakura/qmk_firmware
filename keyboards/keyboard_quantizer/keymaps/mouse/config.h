4337832880b865eab42ec59a3ace7ec23ffbc0ca
kusamakura <882152+kusamakura@users.noreply.github.com>
2022-09-01 19:06:02 +0900

    Add keyboard_quantizer

---
+++ keyboards/keyboard_quantizer/keymaps/mouse/config.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#undef MATRIX_ROWS
+#undef MATRIX_COLS
+
+ // Set 8 buttons + 4 gesture
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 8
+#define MATRIX_MSWHEEL_ROW 1
+#define MATRIX_MSWHEEL_COL 4
+
+// For gesture recognition trick
+#define TAPPING_TERM_PER_KEY
+
+#define USB_MOUSE_POLLING_INTERVAL_MS 1
