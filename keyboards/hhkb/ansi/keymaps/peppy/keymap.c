2039d7ca2e859ff7ae26a8b821f9bc7a185f5b00
kusamakura <882152+kusamakura@users.noreply.github.com>
2023-02-19 13:14:17 +0900

    Add gaming layer to hhkb/ansi

--- keyboards/hhkb/ansi/keymaps/peppy/keymap.c
+++ keyboards/hhkb/ansi/keymaps/peppy/keymap.c
@@ -3,6 +3,8 @@
 #include "peppy.h"
 #include "g/keymap_combo.h"
 
+#define _GMR 4
+
 // clang-format off
 #define LAYOUT_wrapper(...) LAYOUT(__VA_ARGS__)
 
@@ -11,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
     KC_TAB,  ___LEFT_BASE_ROW2___, ___RGHT_BASE_ROW2___, KC_LBRC, KC_RBRC, KC_BSPC,
     KC_LGUI, ___LEFT_BASE_ROW3___, ___RGHT_BASE_ROW3___, KC_SCOLON, KC_ENT,
-    KC_LSFT, ___LEFT_BASE_ROW4___, ___RGHT_BASE_ROW4___, KC_SLSH, KC_RSFT, MO(_NAV),
+    KC_LSFT, ___LEFT_BASE_ROW4___, ___RGHT_BASE_ROW4___, KC_SLSH, TG(_GMR), MO(_NAV),
     KC_LCTL, KC_LALT, NAV_SPC, KC_RALT, KC_RCTL
   ),
 
@@ -37,5 +39,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     VVVV, ___LEFT_NUM_ROW3___, ___RGHT_NUM_ROW3___, XXXX, VVVV,
     VVVV, ___LEFT_NUM_ROW4___, ___RGHT_NUM_ROW4___, VVVV, VVVV, VVVV,
     VVVV, VVVV, VVVV, VVVV, VVVV
+  ),
+
+  [_GMR] = LAYOUT(
+    KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
+    KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
+    KC_LGUI, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TG(_GMR), XXXX,
+    KC_LCTL, KC_LALT, KC_SPC, KC_RALT, KC_RCTL
   )
 };
