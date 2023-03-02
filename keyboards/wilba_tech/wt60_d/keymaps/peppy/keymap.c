97df4f15f31b373591682b600b60d6989ac70150
kusamakura <882152+kusamakura@users.noreply.github.com>
2023-03-03 07:31:20 +0900

    Add kkatano/bakeneko60/keymaps/peppy

--- keyboards/wilba_tech/wt60_d/keymaps/peppy/keymap.c
+++ keyboards/wilba_tech/wt60_d/keymaps/peppy/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		XXXX, XXXX, XXXX, VVVV, XXXX, XXXX, XXXX, XXXX
 	),
 
-	[_GMR] = LAYOUT_all(
+	[_GMR] = LAYOUT_wrapper(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
 		KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
