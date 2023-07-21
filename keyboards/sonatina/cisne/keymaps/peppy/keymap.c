7cc6bb81882c8f7b9d1d81d95041d3638fcd0938
kusamakura <882152+kusamakura@users.noreply.github.com>
2023-07-21 09:31:50 +0900

    Fix Cisne keymap

--- keyboards/sonatina/cisne/keymaps/peppy/keymap.c
+++ keyboards/sonatina/cisne/keymaps/peppy/keymap.c
@@ -8,27 +8,27 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_wrapper(
-    ___LEFT_BASE_ROW2___, XXXX, XXXX, ___RGHT_BASE_ROW2___,
-    ___LEFT_BASE_ROW3___, XXXX, XXXX, ___RGHT_BASE_ROW3___,
-    ___LEFT_BASE_ROW4___, XXXX, XXXX, XXXX, ___RGHT_BASE_ROW4___, KC_SLSH,
+    XXXX, ___LEFT_BASE_ROW2___, ___RGHT_BASE_ROW2___, XXXX,
+    KC_Q, ___LEFT_BASE_ROW3___, ___RGHT_BASE_ROW3___, KC_QUOTE,
+    XXXX, ___LEFT_BASE_ROW4___, XXXX, ___RGHT_BASE_ROW4___, KC_SLSH, XXXX,
            XXXX, XXXX, NAV_SPC, XXXX, NAV_SPC, KC_SPC, XXXX
   ),
   [_NAV] = LAYOUT_wrapper(
-    ___LEFT_NAV_ROW2___, XXXX, XXXX, ___RGHT_NAV_ROW2___,
-    ___LEFT_NAV_ROW3___, XXXX, XXXX, ___RGHT_NAV_ROW3___,
-    ___LEFT_NAV_ROW4___, XXXX, XXXX, XXXX, ___RGHT_NAV_ROW4___, VVVV,
+    XXXX, ___LEFT_NAV_ROW2___, ___RGHT_NAV_ROW2___, XXXX,
+    VVVV, ___LEFT_NAV_ROW3___, ___RGHT_NAV_ROW3___, VVVV,
+    XXXX, ___LEFT_NAV_ROW4___, XXXX, ___RGHT_NAV_ROW4___, VVVV, XXXX,
              XXXX, XXXX, VVVV, XXXX, VVVV, VVVV, QK_BOOT
   ),
   [_SYM] = LAYOUT_wrapper(
-    ___LEFT_SYM_ROW2___, XXXX, XXXX, ___RGHT_SYM_ROW2___,
-    ___LEFT_SYM_ROW3___, XXXX, XXXX, ___RGHT_SYM_ROW3___,
-    ___LEFT_SYM_ROW4___, XXXX, XXXX, XXXX, ___RGHT_SYM_ROW4___, VVVV,
+    XXXX, ___LEFT_SYM_ROW2___, ___RGHT_SYM_ROW2___, XXXX,
+    VVVV, ___LEFT_SYM_ROW3___, ___RGHT_SYM_ROW3___, VVVV,
+    XXXX, ___LEFT_SYM_ROW4___, XXXX, ___RGHT_SYM_ROW4___, VVVV, XXXX,
              XXXX, XXXX, VVVV, XXXX, VVVV, VVVV, XXXX
   ),
   [_NUM] = LAYOUT_wrapper(
-    ___LEFT_NUM_ROW2___, XXXX, XXXX, ___RGHT_NUM_ROW2___,
-    ___LEFT_NUM_ROW3___, XXXX, XXXX, ___RGHT_NUM_ROW3___,
-    ___LEFT_NUM_ROW4___, XXXX, XXXX, XXXX, ___RGHT_NUM_ROW4___, VVVV,
+    XXXX, ___LEFT_NUM_ROW2___, ___RGHT_NUM_ROW2___, XXXX,
+    VVVV, ___LEFT_NUM_ROW3___, ___RGHT_NUM_ROW3___, VVVV,
+    XXXX, ___LEFT_NUM_ROW4___, XXXX, ___RGHT_NUM_ROW4___, VVVV, XXXX,
              XXXX, XXXX, VVVV, XXXX, VVVV, VVVV, XXXX
   ),
 };
